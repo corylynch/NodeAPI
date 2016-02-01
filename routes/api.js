42306542ba74494062d8bf3381e1e3d6d3b2bba4
Cory Lynch <corylynchsrq@gmail.com>
2016-01-31 20:54:28 -0500

    config

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,19 @@
 module.exports = function(express) {
 	var router = express.Router();
 
-	router.get('/', function(req, res) {
+	router.get('./', function(req, res) {
 		res.json({hello: "world"});
 	});
 
-	router.get('/v1', function(req, res) {
+	router.get('./v1', function(req, res) {
 		res.json({hello: "world"});
 	});
 
-	router.get('/status', function(req, res) {
+	router.get('./status', function(req, res) {
 		res.json({healthy: true});
 	});
 
-	router.get('/user/:id', function(req, res) {
+	router.get('./user/:id', function(req, res) {
 		res.json({user: {id: req.params.id}});
 	});
 
