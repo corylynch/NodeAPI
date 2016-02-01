2bbb09a27ca76d3bd8cee30216df98effcda0394
Cory Lynch <corylynchsrq@gmail.com>
2016-01-31 20:47:57 -0500

    config

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 	res.json({hello:"world"});
 });
 
-app.use('/api', require('/routes/api.js')(express));
+app.use('/api', require('../routes/api.js')(express));
  
 var server = app.listen(port, function() {
 	console.log('Server Active on', port);
